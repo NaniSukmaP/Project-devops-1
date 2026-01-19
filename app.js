86dabef44e0927a845c8314af79c03ccf8b11fa7
NaniSukmaP <luckyputri98@gmail.com>
2026-01-19 21:00:28 +0700

    update app.js

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('<h1>Halo Academy! Pipeline CI/CD Berhasil!</h1>');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
